af55e8e1508bf8335d11e7b6944b41b0e4200735
Casper Hooft <casperhooft@hotmail.nl>
2020-02-11 16:20:57 +0100

    Can read database entries inside the client and set new values but doesnt update the database

--- ProductsApp/AProgram.cs
+++ ProductsApp/AProgram.cs
@@ -19,6 +19,101 @@ namespace ProductsApp
 
         }
 
+        static void ReadEntry(DefaultContainer container)
+        {
+            int cleanInput = 0;
+            Console.WriteLine($"Give the ID of the car you wish to read \n");
+
+            string userInput = Console.ReadLine();
+
+
+            while (!int.TryParse(userInput, out  cleanInput))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: \n");
+                userInput = Console.ReadLine();
+            }
+
+            foreach (var p in container.Cars)
+            {
+                if (p.ID == cleanInput)
+                {
+                    Console.WriteLine(p.AmountMade);
+                    Console.WriteLine(p.Brand);
+                    Console.WriteLine(p.Colour);
+                    Console.WriteLine(p.APK);
+                }
+            }
+
+
+            Console.WriteLine($"Your result: read the info of a car \n");
+        }
+
+        static void ChangeEntry(DefaultContainer container)
+        {
+            string userInput = " ";
+            int cleanInput = 0;
+
+            Console.WriteLine($"Give the ID of the car you wish to change \n");
+
+            userInput = Console.ReadLine();
+            
+
+            while (!int.TryParse(userInput, out cleanInput))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: \n");
+                userInput = Console.ReadLine();
+            }
+
+            foreach (var p in container.Cars)
+            {
+                if (p.ID == cleanInput)
+                {
+                    bool answer = false;
+                    string input1 = "";
+                    string colour = "";
+                    int cleanNum1 = 0;
+                    
+                    Console.Write("Type the number of cars made, and then press Enter: \n");
+                    input1 = Console.ReadLine();
+
+                    while (!int.TryParse(input1, out cleanNum1))
+                    {
+                        Console.Write("This is not valid input. Please enter an integer value: \n");
+                        input1 = Console.ReadLine();
+                    }
+
+                    Console.Write("Type the color of the car, and then press Enter: \n");
+                    colour = Console.ReadLine();
+
+                    Console.Write("Is the car APK certifeid? Type true or false, and then press Enter: \n");
+                    input1 = Console.ReadLine().ToLower();
+
+                    if (string.Equals(input1, "true"))
+                    {
+                        answer = true;
+                    }
+                        p.AmountMade = cleanNum1;
+                        p.Brand = SetBrand();
+                        p.Colour = colour;
+                        p.APK = answer;
+                    
+                    Console.WriteLine(p.AmountMade);
+                    Console.WriteLine(p.Brand);
+                    Console.WriteLine(p.Colour);
+                    Console.WriteLine(p.APK);
+
+                    container.UpdateObject(p);
+                    var serviceResponse = container.SaveChanges();
+
+                    foreach (var operationResponse in serviceResponse)
+                    {
+                        Console.WriteLine("Response: {0}", operationResponse.StatusCode);
+                    }
+                }
+            }
+            Console.WriteLine($"Your result: Changed a car \n");
+        }
+
         //set the brand to what the user of the software wants 
         static _Brands SetBrand()
         {
@@ -131,8 +226,6 @@ namespace ProductsApp
 
             Console.Write("Type the color of the car, and then press Enter: \n");
             colour = Console.ReadLine();
-
-            //Console.WriteLine("\tt - Add creation time");
             
             Console.Write("Is the car APK certifeid? Type true or false, and then press Enter: \n");
             input1 = Console.ReadLine().ToLower();
@@ -179,30 +272,42 @@ namespace ProductsApp
                 Console.WriteLine("Choose an option from the following list:");
                 Console.WriteLine("\ta - Add car ");
                 Console.WriteLine("\tc - change a car");
-                Console.WriteLine("\tr - remove a car");
+                Console.WriteLine("\tr - read the info of a car");
+                Console.WriteLine("\tR - remove a car");
                 Console.WriteLine("------------------------\n");
                 Console.Write("Your option? ");
 
                 switch (Console.ReadLine())
                 {
                     default:
+                        //works
                         Console.WriteLine($"Your result: error \n");
                         Console.WriteLine("\n");
                         break;
 
                     case "a":
+                        //works
                         Console.WriteLine("\n");
                         AddCar(container);
                         break;
 
                     case "c":
-                        Console.WriteLine($"Your result: Changed a car \n");
+                        //changes values but doesnt update the database?
+                        Console.WriteLine("\n");
+                        ChangeEntry(container);
                         //search car by id
                         //change car parameters
                         break;
 
                     case "r":
-                        Console.WriteLine($"Your result: Removed a car \n");
+                        //works
+                        Console.WriteLine("\n");
+                        ReadEntry(container);
+                        break;
+
+                    case "R":
+                        //didnt start makeing this yet
+                        Console.WriteLine("\n");
                         //search car by id
                         //remove car
                         break;
